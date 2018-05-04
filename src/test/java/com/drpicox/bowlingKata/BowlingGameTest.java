b041240d2a47ebce06d86f88574ccb0a71699291
porras92 <t.porras07@gmail.com>
2018-05-04 12:13:07 +0200

    Slide 52

--- src/test/java/com/drpicox/bowlingKata/BowlingGameTest.java
+++ src/test/java/com/drpicox/bowlingKata/BowlingGameTest.java
@@ -52,6 +52,13 @@ public class BowlingGameTest extends TestCase {
         assertEquals(24, g.score());
     }
 
+    @Test
+    public void testPerfectGame() throws Exception {
+        rollMany(12,10);
+        assertEquals(300, g.score());
+    }
+
+
     private void rollStrike() {
         g.roll(10);
     }
