014a58e4d707bf3efa73d932bd8e77fec9781af5
porras92 <t.porras07@gmail.com>
2018-05-04 12:07:40 +0200

    Slide 48

--- src/test/java/com/drpicox/bowlingKata/Game.java
+++ src/test/java/com/drpicox/bowlingKata/Game.java
@@ -1,22 +1,25 @@
 package com.drpicox.bowlingKata;
 
 public class Game {
-
-    private int score = 0;
     private int rolls[] = new int[21];
     private int currentRoll = 0;
 
-
     public void roll(int pins) {
         rolls[currentRoll++] = pins;
-
     }
 
     public int score() {
         int score = 0;
         int frameIndex = 0;
         for (int frame = 0; frame < 10; frame++) {
-            if (isSpare(frameIndex)) // spare
+            if (rolls[frameIndex] == 10) // strike
+            {
+                score += 10 +
+                        rolls[frameIndex+1] +
+                        rolls[frameIndex+2];
+                frameIndex++;
+            }
+            else if (isSpare(frameIndex))
             {
                 score += 10 + rolls[frameIndex + 2];
                 frameIndex += 2;
@@ -33,6 +36,5 @@ public class Game {
         return rolls[frameIndex] +
                 rolls[frameIndex + 1] == 10;
     }
-
 }
 
