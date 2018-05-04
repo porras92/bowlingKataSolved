456440b3af005f49ffb03d426d5757d55dbae5bb
porras92 <t.porras07@gmail.com>
2018-05-04 11:53:52 +0200

    Slide 35

--- src/test/java/com/drpicox/bowlingKata/Game.java
+++ src/test/java/com/drpicox/bowlingKata/Game.java
@@ -13,6 +13,10 @@ public class Game {
     }
 
     public int score() {
+        int score = 0;
+        for (int i = 0; i < rolls.length; i++)
+            score += rolls[i];
+
         return score;
     }
 }
