f3ceb4d5db9400be5dbf725073d001c78b861994
porras92 <t.porras07@gmail.com>
2018-05-04 11:30:32 +0200

    Slide 19

--- src/test/java/com/drpicox/bowlingKata/BowlingGameTest.java
+++ src/test/java/com/drpicox/bowlingKata/BowlingGameTest.java
@@ -6,9 +6,19 @@ import org.junit.Test;
 import java.sql.SQLException;
 
 import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
 public class BowlingGameTest {
+
+    @Test
+    public void testGutterGame() throws Exception {
+        Game g = new Game();
+        for (int i=0; i<20; i++)
+            g.roll(0);
+        assertEquals(0, g.score());
+
+    }
 
 
 }
---
+++ src/test/java/com/drpicox/bowlingKata/Game.java
@@ -0,0 +1,10 @@
+package com.drpicox.bowlingKata;
+
+public class Game {
+    public void roll(int i) {
+    }
+
+    public int score() {
+        return 0;
+    }
+}
